938933fe8762f17eb6c2e668cdee6e059ed1721f
sophiagavrila <msophiagavrila@gmail.com>
2021-08-25 13:04:36 -0400

    init project

---
+++ week1/StudentDatabaseApp/src/main/java/com/revature/Driver.java
@@ -0,0 +1,26 @@
+package com.revature;
+
+import java.util.Scanner;
+
+public class Driver {
+
+	// ctrl + shift + o is short cut to import!
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		// Step 1. prompt the user how many students to enter
+		
+		// get the capacity for an array of Student objects
+		
+		// iterate over the array and for each object, we prompt the user for all object details
+		
+		// Step 2. as we create those students, the user is prompted
+		//         to enter the properties of the student
+		
+		// TODO: build a student model and a course model
+		
+
+	}
+
+}
