fc65ff24ccdc3c3e99713ca8d8111190a393cb9f
sophiagavrila <msophiagavrila@gmail.com>
2021-09-01 08:02:08 -0400

    add notes to ConnectionUtil and UserDao insert() method

--- week2/JDBCdemo/src/main/java/com/revature/repositories/UserDao.java
+++ week2/JDBCdemo/src/main/java/com/revature/repositories/UserDao.java
@@ -12,45 +12,62 @@ import com.revature.util.ConnectionUtil;
 public class UserDao implements IUserDao{
 
 	@Override
-	public int insert(User u) {
+	public int insert(User u) { // Think about the User that you take in as being generated from the input that a user gives through the console
 		
 		try {
+			
+			// Capture the single instance of the JDBC Connection (this is called a session)
 			Connection conn = ConnectionUtil.getConnection();
 			
-			// We start with a SQL string
+			// We start with a SQL String -- "RETURNING" allows us to return the PK of the user inserted/created
 			String sql = "INSERT INTO sophiag.users (username, pwd, user_role) VALUES (?, ?, ?) RETURNING sophiag.users.id";
 			
+			/*
+			 * Defend against SQL injection with a Prepared Statement!
+			 * If you don't use a Prepared Statement, a malicious actor could try to insert a statement like "DROP TABLE USERS".
+			 * SQL Injection is typically the #1 Cyber Threat listed by OWASP. https://owasp.org/www-project-top-ten/
+			 */
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			
-			stmt.setString(1, u.getUsername());
+			/**
+			 *  Here we are replacing the ?'s of the sql String with the instance variables of the
+			 *  User u object passed as a parameter to be persisted. 
+			 */
+			stmt.setString(1, u.getUsername());  // The "1" represents the 1st ? in the SQL insertion String
 			stmt.setString(2, u.getPassword());
 			
 			
-			// the enum, is tough because we have to accomodate for a separate custom ENUM type.
+			// Transposing an the ENUM from Java -> SQL, is tough because we have to accommodate for a separate custom ENUM type.
 			stmt.setObject(3,  u.getRole(), Types.OTHER);
 			
-			ResultSet rs; // our SQL stamenet will return a value! We need to iterate over it
+			/**
+			 * When we run stmt.executeQuery(), it returns a ResultSet Object.
+			 * The ResultSet Interface has all of the methods we need to iterate over 
+			 * the returned data.
+			 */
+			ResultSet rs; 
 			
+			// first we check to see if the returned result set is NOT null...
 			if ((rs = stmt.executeQuery()) != null)  {
-				
-				rs.next();
-				
-				int id = rs.getInt(1);
-				
-				return id; // if everything goes well this is where the method returns		
+				// move the cursor of the ResultSet forward 1 with next()
+				rs.next(); 
+				// all we need is the FIRST column's data (which is the PK of the inserted User)
+				int id = rs.getInt(1); // the "1" parameter represents the column of data that we want
+				// if everything goes well, we return that fetched PK	
+				return id; 
 			}
-		
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// TODO add logging!
 			e.printStackTrace();
 		}
 		
-		
-		return -1; // if something doesn't go right, return -1
+		/**
+		 *  Since this method returns an int (as specified in the IUserDao Interface),
+		 *  return -1 if things go wrong inserting a User in the DB, because Users can't
+		 *  have a negative ID 
+		 */
+		return -1; 
 	}
-	
-	
-	
 
 }
--- week2/JDBCdemo/src/main/java/com/revature/util/ConnectionUtil.java
+++ week2/JDBCdemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -7,58 +7,69 @@ import java.sql.DriverManager; // this is JDBC
 import java.sql.SQLException;
 import java.util.Properties;
 
-// Singleton design pattern instantiates an object ONCE
-
 /**
- * Singleton design pattern
+ * Singleton Class is a software design pattern that ensures there will be one
+ * single instance of that class.
  * 
- * -- private constructors
- * -- static field of an instance of the class to be returned
- * -- leverage a public static getInstance() (our getInstance method in this case is the getConnection() method
+ * -- private constructors 
+ * -- static field of an instance of the class to be returned 
+ * -- leverage a public static getInstance() (our getInstance method is the getConnection() method
  * 
- * 
- * @author SophieGavrila
- *
+ * The connection to our DB will be established by a singleton class which will
+ * contain all the necessary driver information (jdbc url, username, password).
  */
 public class ConnectionUtil {
-	
+
 	private static Connection conn = null;
-	
-	// we want to make the constructor PRIVATE 
+
+	/**
+	 *  we want to make the constructor PRIVATE so it can't be instantiated any other way
+	 *  than our getConnection() method
+	 */
 	private ConnectionUtil() {
 		super();
 	}
-	
-	
-	// in a singleton design pattern you create a static "getINstynace() method
+
+	/**
+	 *  The following method is designed to return the ONE single instance of this 
+	 *  class if it exists, or instantiate it if if doesn't.
+	 */
 	public static Connection getConnection() {
-		
-		try {
+
+		try { // check if the ConnectionUtil instance exists or is open first...
 			if (conn != null && !conn.isClosed()) {
 				return conn;
 			}
 		} catch (SQLException e) {
-		// implement an error log here	
+			// TODO: add logging stmt to tell the devs that something went wrong
 			return null;
 		}
-		
-		
+
+		// The Properties object is instantiated to fetch a stream of properties from a file
 		Properties prop = new Properties();
-		
+
 		String url = "";
 		String username = "";
 		String password = "";
-		
+
 		try {
-			prop.load(new FileReader("C:\\Users\\SophieGavrila\\Desktop\\demos\\week2\\JDBCdemo\\src\\main\\resources\\application.properties"));
+			// Use the Properties object to fetch properties from a specified file path (application.properties)
+			prop.load(new FileReader(
+					"C:\\Users\\SophieGavrila\\Desktop\\demos\\week2\\JDBCdemo\\src\\main\\resources\\application.properties"));
 			url = prop.getProperty("url");
 			username = prop.getProperty("username");
 			password = prop.getProperty("password");
-			conn = DriverManager.getConnection(url, username, password);
 			
+			/**
+			 * When the method getConnection is called,the DriverManager will attempt to locate a 
+			 * suitable driver from amongst those loaded at initialization and those loaded 
+			 * explicitly using the same class loader as the current application. 
+			 */
+			conn = DriverManager.getConnection(url, username, password);
+
 			// TODO: Change this to a logging statement once we implement Log4J
 			System.out.println("Connection successful!");
-		
+
 		} catch (IOException e) {
 			// TODO add some logging
 			e.printStackTrace();
@@ -67,7 +78,8 @@ public class ConnectionUtil {
 			System.out.println();
 			return null;
 		}
-
+		
+		// If we establish a connection, return that connection
 		return conn;
 	}
 }
