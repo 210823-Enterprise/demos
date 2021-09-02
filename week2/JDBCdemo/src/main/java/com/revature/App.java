409193ff13155c581843f515ebbee596088446c3
sophiagavrila <msophiagavrila@gmail.com>
2021-09-01 23:49:10 -0400

    change line 82 in AccountDao to account not acc_owner

--- week2/JDBCdemo/src/main/java/com/revature/App.java
+++ week2/JDBCdemo/src/main/java/com/revature/App.java
@@ -1,27 +1,11 @@
 package com.revature;
 
-import java.util.ArrayList;
-
-import com.revature.models.Account;
-import com.revature.models.Role;
-import com.revature.models.User;
-import com.revature.services.IUserService;
-import com.revature.services.UserService;
-
 public class App {
 
 	public static void main(String[] args) {
 		
-		User u1 = new User("Batman", "pass", Role.Admin, new ArrayList<Account>());
-
 		
-		System.out.println("please enter your username");
 		
-		// scanner gets the username, and password.... and we build a new User obj from the console 
-		
-		// call our service method to REGISTER it
-		IUserService userv = new UserService();
-		userv.register(new User("Spiderman", "password", Role.Customer, new ArrayList<Account>()));		
 	}
 
 }
