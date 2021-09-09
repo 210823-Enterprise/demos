6e76cc10f3b83a92e757c985b3dad38fd15f3be3
sophiagavrila <msophiagavrila@gmail.com>
2021-09-09 11:32:33 -0400

    add producer/consumer problem finish

--- week3/Multithreading/src/main/java/com/revature/producerconsumer/Consumer.java
+++ week3/Multithreading/src/main/java/com/revature/producerconsumer/Consumer.java
@@ -3,10 +3,21 @@ package com.revature.producerconsumer;
 public class Consumer implements Runnable{
 	
 	public Holder data;
+	int max;
+	
+	public Consumer(Holder data, int max) {
+		super();
+		this.data = data;
+		this.max = max;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		for (int i=0; i< max; i++) {
+			int currentVal = this.data.getVal();
+			
+			System.out.println("Consumer got: " + currentVal);
+		}
 		
 	}
 
