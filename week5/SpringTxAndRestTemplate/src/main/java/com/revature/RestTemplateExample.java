43e44284b343beeb7dba166558f89b4342ce7b1b
sophiagavrila <msophiagavrila@gmail.com>
2021-09-28 10:47:35 -0400

    complete RestTemplate get vs. post

--- week5/SpringTxAndRestTemplate/src/main/java/com/revature/RestTemplateExample.java
+++ week5/SpringTxAndRestTemplate/src/main/java/com/revature/RestTemplateExample.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate; // RestTemplate is Spring 5, WebCLient Service is now used
 
-public class RestTemplateExample {
+public class RestTemplateExample { // https://howtodoinjava.com/spring-boot2/resttemplate/spring-restful-client-resttemplate-example/
 	
 	// Goal: we want to use the RestTemplate to hit an end point exposed in the FoodController
 	
@@ -51,8 +51,24 @@ public class RestTemplateExample {
 		// 5. print the response to the console. 
 		System.out.println(httpResponse);
 		
-		
 		// To run: run spring boot app, then run this class as a Java App
+		
+		/**
+		 * GET
+		 */
+		
+		// 1. Initialize new URL for GET request
+		String getUrl = url + "/all"; // "http://localhost:5000/food/all"
+		
+		// 2. Make a new http entity to get the result as a string
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		// 3. Send the GET  request with exchange method
+		ResponseEntity<String> getResponse = restTemplate.exchange(getUrl, HttpMethod.GET, entity, String.class);
+		
+		// 4. print out the JSON response
+		System.out.println(getResponse);
+		
 	}
 
 	
