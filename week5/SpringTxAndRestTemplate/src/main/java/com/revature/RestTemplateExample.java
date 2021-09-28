bb5eb833db3314f910e7c0a087cab45d6495de29
sophiagavrila <msophiagavrila@gmail.com>
2021-09-28 10:38:00 -0400

    add REstTemplate example

---
+++ week5/SpringTxAndRestTemplate/src/main/java/com/revature/RestTemplateExample.java
@@ -0,0 +1,61 @@
+package com.revature;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateExample {
+	
+	// Goal: we want to use the RestTemplate to hit an end point exposed in the FoodController
+	
+	static String url = "http://localhost:5000/food"; // this will point to my RestController
+	
+	public static void main(String[] args) {
+
+		// 1. Instantiate the RestTemplate object
+		RestTemplate restTemplate = new RestTemplate();
+		
+		// we need to call the .exchange() method allows 
+		// us to exchange data with a server / GET, POST, DELETE< PUT, PATCH
+		
+		// 2. Build the HTTP Headers to send our request
+		HttpHeaders headers = new HttpHeaders();
+		// we want to accept back JSON, so we set our Headers to accept JSON as the response
+		headers.setAccept(Arrays.asList(new MediaType[] {MediaType.APPLICATION_JSON}));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		/**
+		 * POST request (CREATE a food object in the server)
+		 */
+		
+		// 1. send the food object as a JSON String (send the object as a Food Object)
+		String food = "{\r\n" + 
+				"    \"dishName\" : \"sushi\",\r\n" + 
+				"    \"calories\" : \"200\"\r\n" + 
+				"}";
+		
+		// 2. define the URL pointing to WHERE we're sending the post request (http://localhost:5000/food/add)
+		String postUrl = url + "/add"; // (http://localhost:5000/food/add)
+		
+		// 3. Build an HTTP Entity (this represents the request we're sending to the server (body + headers)
+		HttpEntity<String> httpEntity = new HttpEntity<String>(food, headers);
+		
+		// 4. call the exchange() method to fire the request!
+		ResponseEntity<String> httpResponse = restTemplate.exchange(postUrl, HttpMethod.POST, httpEntity, String.class);
+		 
+		// 5. print the response to the console. 
+		System.out.println(httpResponse);
+		
+		
+		// To run: run spring boot app, then run this class as a Java App
+	}
+
+	
+	
+
+}
--- week5/SpringTxAndRestTemplate/src/main/java/com/revature/SpringTxAndRestTemplateApplication.java
+++ week5/SpringTxAndRestTemplate/src/main/java/com/revature/SpringTxAndRestTemplateApplication.java
@@ -11,21 +11,5 @@ public class SpringTxAndRestTemplateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTxAndRestTemplateApplication.class, args);
 	}
-	
-	
-	/**
-	 * Create Bean for Rest Template to auto 
-	 * wire the Rest Template object into our controller.
-	 * 
-	 * RestTemplate is the central class within the SpringFramework 
-	 * for executing synchronous HTTP requests on the client side
-	 * 
-	 * Spring's REST web-client
-	 */
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-	
 
 }
